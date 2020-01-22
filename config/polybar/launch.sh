34d54bca9af94710ed2fa8d2b74ef93885699cc7
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-01-22 20:14:41 -0300

    adding polybar

---
+++ config/polybar/launch.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+desktop=$(echo $DESKTOP_SESSION)
+count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+
+case $desktop in
+
+i3)
+    if type "xrandr" >/dev/null; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            MONITOR=$m polybar --reload mainbar-i3 -c ~/.config/polybar/config-main.ini &
+        done
+    else
+        polybar --reload mainbar-i3 -c ~/.config/polybar/config-main.ini &
+    fi
+    # second polybar at bottom
+    if type "xrandr" >/dev/null; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            MONITOR=$m polybar --reload mainbar-i3-extra -c ~/.config/polybar/config-extra.ini &
+        done
+    else
+        polybar --reload mainbar-i3-extra -c ~/.config/polybar/config-extra.ini &
+    fi
+    ;;
+
+bspwm)
+    if type "xrandr" >/dev/null; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config-main.ini &
+        done
+    else
+        polybar --reload mainbar-bspwm -c ~/.config/polybar/config-main.ini &
+    fi
+    # second polybar at bottom
+    if type "xrandr" >/dev/null; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            MONITOR=$m polybar --reload mainbar-bspwm-extra -c ~/.config/polybar/config-extra.ini &
+        done
+    else
+        polybar --reload mainbar-bspwm-extra -c ~/.config/polybar/config-extra.ini &
+    fi
+    ;;
+esac
