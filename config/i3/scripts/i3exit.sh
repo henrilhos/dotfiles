ea16044f7a267d348a57378067d2cdd1747014fc
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-01-28 13:43:38 -0300

    chore: update i3 configuration

---
+++ config/i3/scripts/i3exit.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+case "$1" in
+lock)
+    slimlock
+    #dm-tool lock
+    ;;
+logout)
+    i3-msg exit
+    ;;
+suspend)
+    systemctl suspend
+    ;;
+hibernate)
+    systemctl hibernate
+    ;;
+reboot)
+    systemctl reboot
+    ;;
+shutdown)
+    systemctl poweroff
+    ;;
+*)
+    echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+    exit 2
+    ;;
+esac
+
+exit 0
