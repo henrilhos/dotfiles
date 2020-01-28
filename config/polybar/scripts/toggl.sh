d10c27e2cf8eb6d83908defdffbec963564191a4
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-01-28 10:53:40 -0300

    adding toggl module

---
+++ config/polybar/scripts/toggl.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+main() {
+    if ! [ -f ~/.config/polybar/scripts/toggl_key ]; then
+        echo ""
+        exit
+    fi
+
+    api_token=$(cat ~/.config/polybar/scripts/toggl_key)
+    api_url="https://www.toggl.com/api/v8/time_entries/current"
+
+    result=$(curl -su $api_token:api_token -X GET $api_url)
+
+    if [ "$(echo $result | jq -r '.data')" = "null" ]; then
+        echo ""
+        exit
+    fi
+
+    start_date=$(echo $result | jq -r '.data.start')
+    description=$(echo $result | jq -r '.data.description')
+
+    diff=$(($(date "+%s") - $(date -d $start_date "+%s")))
+
+    duration=$(printf '%dh:%dm\n' $(($diff / 3600)) $(($diff % 3600 / 60)))
+
+    echo "${*:-%duration% - %description%}" | sed "s/%duration%/$duration/g;s/%description%/$description/g"i | sed 's/&/\\&/g'
+}
+
+main "$@"
