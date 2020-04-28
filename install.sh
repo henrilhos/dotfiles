dccd2444c71c9836f77fab0b4d9ede7a452170b1
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-04-28 17:42:35 -0300

    feat: adding programs

--- install.sh
+++ install.sh
@@ -29,5 +29,37 @@ mkdir build && cd build
 make
 sudo make install
 
+# Install DE programs
+sudo apt install -y i3-gaps-wm i3xrocks i3-snapshot i3ipc-python i3status rofi dmenu feh
+
+# Install interface programs
+# Visual Studio Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt install -y apt-transport-https
+
+# Vivaldi
+sudo sh -c 'echo "deb http://repo.vivaldi.com/stable/deb/ stable main" >> /etc/apt/sources.list'
+wget -q -O - http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
+
+# Brave
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# Telegram
+sudo add-apt-repository ppa:atareao/telegram
+
+# Spotify
+sudo sh -c "echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list.d/spotify.list"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+
+sudo apt update
+sudo apt install -y kitty code vivaldi-stable firefox brave-browser slack-desktop \
+	snapd telegram spotify-client
+
+sudo snap install rocketchat-desktop
+
 # Runs autoremove
 sudo apt autoremove -y
