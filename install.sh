11ff2c2dd273ad1217428a74264c1552f34ad47f
Murilo de Oliveira do Nascimento <murilooon@gmail.com>
2020-06-25 21:21:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo apt update
 sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
 	libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev \
 	libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev \
-	libxbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-xrm-dev libxcb-shape0-dev
+	libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-xrm-dev libxcb-shape0-dev
 
 cd /tmp
 git clone https://github.com/Airblader/i3 i3-gaps
