a0f2677d8438d32fd51862bd79f5b68bc869e9ad
Henrique de Castilhos <hello@henrique.zip>
2025-06-24 21:22:40 -0300

    fix: update scripts permissions

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -166,13 +166,13 @@ defaults write com.apple.dock dashboard-in-overlay -bool true
 # defaults write com.apple.dock mru-spaces -bool false
 
 # Remove the auto-hiding Dock delay
-# defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-delay -float 0
 
 # Remove the animation when hiding/showing the Dock
-# defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
 
 # Automatically hide and show the Dock
-# defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide -bool true
 
 # Make Dock icons of hidden applications translucent
 defaults write com.apple.dock showhidden -bool true
