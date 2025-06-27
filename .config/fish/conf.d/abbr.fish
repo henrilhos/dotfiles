75171b08ede9ac067cd10561d751e8bb6d905996
Henrique de Castilhos <hello@henrique.zip>
2025-06-27 17:42:52 -0300

    feat: add Fish configuration

---
+++ .config/fish/conf.d/abbr.fish
@@ -0,0 +1,55 @@
+abbr :bd exit
+abbr :q "tmux kill-server"
+abbr :qa! "tmux kill-server"
+
+abbr bi "brew install"
+abbr bic "brew install --cask"
+abbr bin "brew info"
+abbr binc "brew info --cask"
+abbr bl "brew leaves"
+abbr blr "brew leaves --installed-on-request"
+abbr blp "brew leaves --installed-as-dependency"
+abbr bs "brew search"
+
+abbr c clear
+abbr cl clear
+abbr claer clear
+abbr clera clear
+abbr cx "chmod +x"
+
+abbr d dev
+abbr dc "docker compose"
+abbr dcd "docker compose down"
+abbr dcdv "docker compose down -v"
+abbr dcr "docker compose restart"
+abbr dcu "docker compose up -d"
+abbr dps "docker ps --format 'table {{.Names}}\t{{.Status}}'"
+
+abbr e exit
+
+abbr fi "fisher install"
+abbr fu "fisher update"
+abbr fl "fisher list | sed 's/.*/"&/"'"
+
+abbr ld lazydocker
+abbr lg lazygit
+
+abbr nb "npm run build"
+abbr nd "npm run dev"
+abbr ne "nvim .env"
+abbr nf neofetch
+abbr ni "npm install"
+abbr nt "npm run test"
+
+abbr o "open ."
+
+abbr rmr "rm -rf"
+
+abbr s sesh_start
+abbr s. "sesh connect ."
+abbr sc "sesh clone --cmdDir ~/c (pbpaste)"
+abbr sf "source ~/.config/fish/config.fish"
+abbr sr "sesh root"
+
+abbr ta "tmux attach"
+abbr tk "tmux kill-server"
