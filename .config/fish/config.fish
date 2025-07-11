447c7ebdfd3b4615bebec7078861e7b4b1c4b798
Henrique de Castilhos <hello@henrique.zip>
2025-07-11 11:09:27 -0300

    chore: update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,4 +15,22 @@ set -U fish_key_binding fish_vi_key_bindings
 set -Ux EDITOR nvim # 'neovim/neovim' text editor
 set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
 
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 fish_add_path $HOME/.config/bin # my custom scripts
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
