9eaaf9949a20962e9c5acf1dde92db2b45473f9f
Henrique de Castilhos <hello@henrique.zip>
2026-03-25 11:25:21 -0300

    feat: add neovim config

---
+++ dotfiles/.config/nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options herev
+vim.opt.spell = true
+vim.opt.spelllang = { "en", "pt_br" }
