d010faac88b7cbef3b1e2223ab7babb03871722b
Henrique de Castilhos <hello@henrique.zip>
2026-09-07 21:58:50 -0300

    feat(nvim): improve line wrap, add spell check, disable editorconfig newline override

--- configs/nvim/lua/config/options.lua
+++ configs/nvim/lua/config/options.lua
@@ -2,4 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.wrap = true
+-- Line wrapping
+vim.opt.wrap = true -- wrap long lines instead of scrolling horizontally
+vim.opt.linebreak = true -- wrap at word boundaries instead of mid-word
+vim.opt.breakindent = true -- keep wrapped lines visually indented to match the start of the line
+
+-- Spell check
+vim.opt.spell = true -- enable spell checking
+vim.opt.spelllang = { "en", "pt_br" } -- check spelling against English and Brazilian Portuguese
+
+-- Editorconfig overrides
+-- disable insert_final_newline so editorconfig doesn't force/strip trailing newlines
+require("editorconfig").properties.insert_final_newline = function() end
